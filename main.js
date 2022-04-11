df81cf2b7a5da4461487a1b2bd05d7ee53d88ef5
Christina Wilson <WilsonCN14@gmail.com>
2022-04-11 13:49:14 -0500

    Added html, css, js, and image files

---
+++ main.js
@@ -0,0 +1,7 @@
+var popup = document.getElementById("popupStore");
+var openButton = document.getElementById("openPopup");
+var closeButton = document.getElementById("closePopup");
+
+openButton.onclick = function() {
+    document.body.style.background = "green";
+}
